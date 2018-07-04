4c8768eb8c278223cdea949b59a033b2a4b76bb7
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-04 20:23:41 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string) #Take in a string
+  # Convert string into an array
+  words = string.split
+  # Iterate over the array using each
+  # words.each do |word|
+  #   # Reverse each word [desctrucively]!
+  #   word.reverse!
+  # end
+  # Iterate over the array
+  words.collect { |word| word.reverse! } # Reverse each word [desctrucively]!
+  # Join the words into a new string
+  words.join(' ')
+end
